58de2b45ffe6ddccc2a382b5730f61b2ac4f6ab0
jimmy90709 <57282807+jimmy90709@users.noreply.github.com>
2019-11-16 17:00:53 +0800

    認識API

---
+++ Unity/Assets/scripts/LearnAPI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    public Transform transformA;
+    public Transform transformB;
+    public Camera depthA;
+
+    private void Start()
+    {
+         // 使用靜態成員 - 屬性
+         print("隨機 :" + Random.value);
+        
+         //使用靜態成員 數學類別.PI (Mathf)
+         print("PI:" + Mathf.PI);
+
+         // 使用靜態成員 - 方法
+          print("隨機範圍:" + Random.Range(0,100));
+
+          print("絕對值:" + Mathf.Abs(-99));
+
+          //非靜態成員
+          //需要先給予蘭為存放時體物件
+          print("物件 A 的座標:" + transformA.position);
+        
+          transformB.position = new Vector3(-2,0,0);
+          print("物件 B 的座標:" + transformB.position);
+
+          print("攝影機深度:" + depthA.depth);
+
+    }
+     private void Update()
+    {
+        //非靜態成員 - 方法
+        transformB.Rotate(0,0,10);
+    }
+}
---
+++ Unity/Assets/scripts/LearnIF.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class LearnIF : MonoBehaviour
+{
+
+    public bool test;
+    public string prop;
+    public int HP = 100;
+
+    //更新事件:一秒執行 60 次
+    private void Update()
+    {
+        #region  練習 if
+        //判斷式 if 語法
+        // if (布林植) { 陳述式或演算法 }
+        // () 裡面的布林植為 true 才會執行
+        if (test)
+        {
+            print("打開開關");
+        }
+        // () 裡面的布林植為 false 才會執行 else
+        else
+        {
+            print("關閉開關~");
+        }
+
+        if(prop == "紅色藥水")
+        {
+            print("補血");
+        }
+        else if(prop == "藍色藥水")
+        {
+            print("補魔力");
+        }
+        else if(prop == "黃色藥水")
+        {
+            print("補體力");
+        }
+        else
+        {
+            print("沒發生任何事~");
+        }
+        #endregion
+
+        if(HP>=70)
+        {
+            print("安全");
+        }
+        else if(HP>=50)
+        {
+            print("警告");
+        }
+        else if(HP>=20)
+        {
+            print("危險");
+        }
+        else if(HP>0)
+        {
+            print("快死了");
+        }
+        else
+        {
+            print("大俠勝敗乃兵家常事~ 請重新來過");
+        }
+    }
+}
